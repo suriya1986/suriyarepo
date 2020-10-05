5880d26a509bec4470bb250cf6e717f58e66dc01
Suriyanarayanan Ramanujam <suriya1986@gmail.com>
2020-10-05 22:13:52 +0530

    tf updated

--- terraform/build/main.tf
+++ terraform/build/main.tf
@@ -6,25 +6,34 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  # TODO: change this to your own name! S3 bucket names must be *globally* unique.
-  bucket = "suriya-tf-state-bucket"
+resource "aws_instance" "web" {
+  ami           = "ami-0e306788ff2473ccb"
+  instance_type = "t2.micro"
 
-  # Enable versioning so we can see the full revision history of our
-  # state files
-  versioning {
-    enabled = true
-  }
-
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
+  tags = {
+    Name = "HelloWorld"
   }
 }
+
+# resource "aws_s3_bucket" "terraform_state" {
+#   # TODO: change this to your own name! S3 bucket names must be *globally* unique.
+#   bucket = "suriya-tf-state-bucket"
+
+#   # Enable versioning so we can see the full revision history of our
+#   # state files
+#   versioning {
+#     enabled = true
+#   }
+
+#   # Enable server-side encryption by default
+#   server_side_encryption_configuration {
+#     rule {
+#       apply_server_side_encryption_by_default {
+#         sse_algorithm = "AES256"
+#       }
+#     }
+#   }
+# }
 
 # resource "aws_s3_bucket" "b" {
 #   bucket = "my-tf-test-bucket"
