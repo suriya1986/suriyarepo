2c409faf49c06e5bb7b3763b449c3d55bc16711d
Suriyanarayanan Ramanujam <suriya1986@gmail.com>
2021-01-26 12:49:31 +0530

    fixed key

--- terraform/build/main.tf
+++ terraform/build/main.tf
@@ -2,6 +2,7 @@ terraform {
   required_version = ">= 0.13"
    backend "s3" {
       bucket = "suriya-build-artifacts"
+      key    = "myapp.tfstate"
       workspace_key_prefix="myapp"
    }
   # backend "s3" {
