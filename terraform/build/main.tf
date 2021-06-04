26a77aa1d8c70dc0faa4528bcad61337a4fb046e
Suriyanarayanan Ramanujam <suriya1986@gmail.com>
2021-06-04 21:46:24 +0530

    tf

--- terraform/build/main.tf
+++ terraform/build/main.tf
@@ -20,8 +20,7 @@ variable "environment" {
   
 }
 
-resource "aws_sns_topic" "topic"
-{
+resource "aws_sns_topic" "user_updates" {
   name                        = "user-updates-topic.fifo"
   fifo_topic                  = true
   content_based_deduplication = true
